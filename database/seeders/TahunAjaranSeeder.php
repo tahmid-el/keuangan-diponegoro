7add224bad7856604415ff67dc7ba4977e1727d0
rizaoktorabp <rizaoktora22@gmail.com>
2026-07-25 14:14:51 +0700

    perbaiki seeder untuk tahun ajaran

--- database/seeders/TahunAjaranSeeder.php
+++ database/seeders/TahunAjaranSeeder.php
@@ -13,13 +13,15 @@ class TahunAjaranSeeder extends Seeder
             'nama' => '2025/2026',
             'tanggal_mulai' => '2025-07-14',
             'tanggal_selesai' => '2026-06-30',
+            'semester'=>'Ganjil',
             'is_aktif' => true,
         ]);
-
+        
         TahunAjaran::create([
             'nama' => '2024/2025',
             'tanggal_mulai' => '2024-07-15',
             'tanggal_selesai' => '2025-06-30',
+            'semester'=>'Ganjil',
             'is_aktif' => false,
         ]);
     }
