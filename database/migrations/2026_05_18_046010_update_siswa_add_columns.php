e9da15357517cf3961a3197b432e3af29d581095
rizaoktorabp <rizaoktora22@gmail.com>
2026-07-25 14:22:08 +0700

    update migrasi database

--- database/migrations/2026_05_18_046010_update_siswa_add_columns.php
+++ database/migrations/2026_05_18_046010_update_siswa_add_columns.php
@@ -9,20 +9,34 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('siswa', function (Blueprint $table) {
-            if(!Schema::hasColumn('siswa', 'jenis_kelamin')) {
+
+            if (!Schema::hasColumn('siswa', 'jenis_kelamin')) {
                 $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan'])
-                      ->default('Laki-laki')
-                      ->after('telepon');
-    
-                $table->string('nama_ortu')->nullable()->after('jenis_kelamin');
+                    ->default('Laki-laki')
+                    ->after('telepon');
             }
+
+            if (!Schema::hasColumn('siswa', 'nama_ortu')) {
+                $table->string('nama_ortu')
+                    ->nullable()
+                    ->after('jenis_kelamin');
+            }
+
         });
     }
 
     public function down(): void
     {
         Schema::table('siswa', function (Blueprint $table) {
-            $table->dropColumn(['jenis_kelamin', 'nama_ortu']);
+
+            if (Schema::hasColumn('siswa', 'nama_ortu')) {
+                $table->dropColumn('nama_ortu');
+            }
+
+            if (Schema::hasColumn('siswa', 'jenis_kelamin')) {
+                $table->dropColumn('jenis_kelamin');
+            }
+
         });
     }
 };
